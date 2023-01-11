f577e9d29357007222164ad3503a984cf165d233
George Vagenas <gvagenas@gmail.com>
2023-01-11 21:29:22 +0200

    Initial commit

--- main.tf
+++ main.tf
@@ -1,8 +1,12 @@
 provider "aws" {
-  region = "us-west-2"
+  region                      = "us-east-1"
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "ec2_instance_example" {
   ami           = "ami-0abe92d15a280b758"
   instance_type = "t2.micro"
 
@@ -11,11 +15,11 @@ resource "aws_instance" "example" {
   }
 }
 
-resource "aws_s3_bucket" "example_bucket" {
+resource "aws_s3_bucket" "s3_example_bucket" {
   bucket = "example-bucket"
 }
 
-resource "aws_db_instance" "example" {
+resource "aws_db_instance" "rds_example" {
   engine                  = "postgres"
   instance_class          = "db.t2.micro"
   name                    = "example-db"
